663db01abf02631ba234f70604b01c3a84614b4f
tn-Kazuto <ngotrangvinh2308@gmail.com>
2025-12-12 10:30:48 +0700

    Add files via upload

---
+++ EXAM_SQL.sql
@@ -0,0 +1,67 @@
+﻿
+CREATE DATABASE ABC_Company_Payroll;
+GO
+USE ABC_Company_Payroll;
+GO
+
+
+CREATE TABLE Departments (
+    DepartmentCode VARCHAR(10) PRIMARY KEY,
+    DepartmentName NVARCHAR(50) 
+);
+
+
+CREATE TABLE Employees (
+    EmployeeCode VARCHAR(10) PRIMARY KEY,
+    EmployeeName NVARCHAR(100),
+    DepartmentCode VARCHAR(10),
+    BasicSalary DECIMAL(18, 2),
+    FOREIGN KEY (DepartmentCode) REFERENCES Departments(DepartmentCode)
+);
+
+CREATE TABLE Payroll (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeCode VARCHAR(10),
+    PayrollMonth INT,
+    WorkingDays INT,
+    OffDaysWithPay INT,
+    OffDaysWithoutPay INT,
+    GrossSalary DECIMAL(18, 2),
+    NetSalary DECIMAL(18, 2),
+    Note NVARCHAR(255),
+    FOREIGN KEY (EmployeeCode) REFERENCES Employees(EmployeeCode)
+);
+GO
+
+
+INSERT INTO Departments (DepartmentCode, DepartmentName) VALUES 
+('IT', N'Công nghệ thông tin'),
+('HR', N'Nhân sự'),
+('SALE', N'Kinh doanh');
+
+
+INSERT INTO Employees (EmployeeCode, EmployeeName, DepartmentCode, BasicSalary) VALUES
+('A1', N'Nguyễn Văn A', 'IT', 1000),
+('A2', N'Lê Thị Bình', 'IT', 1200),
+('B1', N'Nguyễn Lan', 'HR', 600),
+('D1', N'Mai Tuấn Anh', 'HR', 500),
+('C1', N'Hà Thị Lan', 'HR', 500),
+('C2', N'Lê Tú Chinh', 'SALE', 1200),
+('D2', N'Trần Văn Toàn', 'HR', 500),
+('A3', N'Trần Văn Nam', 'IT', 1200),
+('B2', N'Huỳnh Anh', 'SALE', 1200);
+
+INSERT INTO Payroll (EmployeeCode, PayrollMonth, WorkingDays, OffDaysWithPay, OffDaysWithoutPay, GrossSalary, NetSalary, Note) VALUES
+('A1', 10, 22, 0, 0, 22000, 20000, NULL),
+('A2', 10, 21, 1, 0, 26400, 23000, NULL),
+('B1', 10, 20, 1, 1, 13200, 12000, NULL),
+('D1', 10, 20, 1, 1, 11000, 10000, NULL),
+('C1', 10, 22, 0, 0, 11000, 10000, NULL),
+('C2', 10, 22, 0, 0, 26400, 23000, NULL),
+('D2', 10, 22, 0, 0, 11000, 10000, NULL),
+('A3', 10, 22, 0, 0, 26400, 23000, NULL),
+('B2', 10, 21, 1, 0, 26400, 23000, NULL);
+GO
+
+EXEC CalculateTotalSalaryByDepartment
+EXEC GetFullPayrollReport;
---
+++ EXAM_SQL_STORE_PROCEDURES.sql
@@ -0,0 +1,69 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE CalculateTotalSalaryByDepartment
+AS
+BEGIN
+    SELECT 
+        d.DepartmentCode,
+        SUM(p.NetSalary) AS TotalNetSalary,
+        SUM(p.GrossSalary) AS TotalGrossSalary 
+    FROM 
+        Departments d
+    JOIN 
+        Employees e ON d.DepartmentCode = e.DepartmentCode
+    JOIN 
+        Payroll p ON e.EmployeeCode = p.EmployeeCode
+    WHERE 
+        p.PayrollMonth = 10 
+    GROUP BY 
+        d.DepartmentCode 
+    ORDER BY 
+        d.DepartmentCode ASC; 
+END;
+GO
+
+CREATE PROCEDURE GetFullPayrollReport
+    @Month INT = 10
+AS
+BEGIN
+    SELECT 
+        e.EmployeeCode AS [Employee code],
+        e.EmployeeName AS [Employee name],
+        e.DepartmentCode AS [Department code],
+        p.WorkingDays AS [Number of working day],
+        p.OffDaysWithPay AS [Number of days off with pay],
+        p.OffDaysWithoutPay AS [Number of days off without pay],
+        FORMAT(e.BasicSalary, '#,##0') AS [Basic salary], -- Format số cho đẹp giống ảnh (tuỳ chọn)
+        FORMAT(p.GrossSalary, '#,##0') AS [Gross salary],
+        FORMAT(p.NetSalary, '#,##0') AS [Net Salary],
+        p.Note AS [Note]
+    FROM 
+        Employees e
+    INNER JOIN 
+        Payroll p ON e.EmployeeCode = p.EmployeeCode
+    INNER JOIN 
+        Departments d ON e.DepartmentCode = d.DepartmentCode
+    WHERE 
+        p.PayrollMonth = @Month;
+END;
+GO
+
+GO
